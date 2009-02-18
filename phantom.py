3c8a5b0d8a5af64631db31ef6c9e14d20d30525e
Sameer Rahmani <lxsameer@debian.(none)>
2009-02-19 03:27:44 +0330

    Finishing basic command reading

--- phantom.py
+++ phantom.py
@@ -20,6 +20,29 @@ import time
 
 
 
+
+
+
+def analyze (msg , nick , s):
+    """analyze msg for a valide command"""
+    
+    if msg[0] == "!":
+        command = msg[1:]
+        # i must add dest channel
+        # write proc needed
+        if msg[1:] == "version\r\n":
+            print "Recieved Version command."
+            s.send ("PRIVMSG #alavnet : %s \r\n" % ( "Phantom bot version 0.1.0"))
+        elif msg[1:] == "help\r\n":
+            print "Recieved help command."
+            s.send ("PRIVMSG #alavnet : %s \r\n" % ( "nothing ."))
+        else:
+           
+            s.send ("PRIVMSG #alavnet : %s \r\n" % ( "I can't understand you . please refer to help section."))
+    return 0
+
+
+
 argnick = "Phantom_bot"
 argident = ""
 argowner = "Sameer Rahmani"
@@ -67,16 +90,16 @@ try:
         while 1:
             data = s.recv(1024)
             if data:
-                print "data: " + unicode ( data )
+                print "data: " + unicode ( data)
                 readbuffer = readbuffer + data
                 temp = string.split ( readbuffer , "\n" )
                 readbuffer = temp.pop( )
                 
-                for line in temp:
-                    line = string.rstrip ( line )
-                    line = string.split ( line )  
-
-                print "\ntemp: %s" % temp 
+                if data.find ( 'PRIVMSG' ) != -1:
+                    nick = data.split ( '!' ) [ 0 ].replace ( ':', '' )
+                    message = ':'.join ( data.split ( ':' ) [ 2: ] )
+                    print nick + ':', message
+                    analyze ( message , nick , s)
 
 except :
     pass
